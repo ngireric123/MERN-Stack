9b3f6081c17645caa58d857cea902fdd0250687a
ngireric123 <ngireric123@gmail.com>
2022-09-09 12:28:13 +0200

    add notes models

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))//express.static bui
 
 app.use('/', require('./routes/root'))
 app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
 
 app.all('*', (req, res) => {
     res.status(404)
