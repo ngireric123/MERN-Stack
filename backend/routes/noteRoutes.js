97350f07a5162c51425a29e303f1c015ab876c8b
ngireric123 <ngireric123@gmail.com>
2022-09-18 20:10:04 +0200

    adding Auth

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const notesController = require('../controllers/notesController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+// this applied to all routes inside this file
+router.use(verifyJWT)
 
 router.route('/')
     .get(notesController.getAllNotes)
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+// this applied to all routes inside this file
+router.use(verifyJWT)
 
 router.route('/')
     .get(usersController.getAllUsers)
